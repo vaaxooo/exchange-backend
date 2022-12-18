bf075b0522335664a8b94863225a82746956e200
vaaxooo <donjkee47@gmail.com>
2022-12-18 14:23:49 +0100

    fix

--- database/migrations/2022_12_18_131947_add_place_passport.php
+++ database/migrations/2022_12_18_131947_add_place_passport.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('verification', function (Blueprint $table) {
+        Schema::table('verifications', function (Blueprint $table) {
             $table->string('place_passport')->nullable();
         });
     }
@@ -25,7 +25,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('verification', function (Blueprint $table) {
+        Schema::table('verifications', function (Blueprint $table) {
             //
         });
     }
